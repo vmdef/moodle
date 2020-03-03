cf70c3dace94515d47511c78cb403585a8f95e8a
Victor Deniz Falcon <victor@moodle.com>
2020-03-03 12:17:30 +0000

    MDL-67814 core_h5p: implemented H5P Core/Editor library interfaces

---
+++ h5p/tests/editor_framework_test.php
@@ -0,0 +1,162 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Testing the H5peditorStorage interface implementation.
+ *
+ * @package    core_h5p
+ * @category   test
+ * @copyright  2020 Victor Deniz <victor@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace core_h5p;
+
+use core_h5p\local\library\autoloader;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ *
+ * Test class covering the H5peditorStorage interface implementation.
+ *
+ * @package    core_h5p
+ * @copyright  2020 Victor Deniz <victor@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @runTestsInSeparateProcesses
+ */
+class editor_framework_testcase extends \advanced_testcase {
+
+    /** @var editor_framework H5P editor framework instance */
+    protected $editor_framework;
+
+    /**
+     * Set up function for tests.
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        autoloader::register();
+
+        $this->editor_framework = new editor_framework();
+    }
+
+    /**
+     * Test that the method saveFileTemporarily saves a file in a temporary location.
+     */
+    public function test_saveFileTemporarily() {
+        $this->resetAfterTest(true);
+
+        // Create temp folder.
+        $tempfolder = make_request_directory(false);
+
+        // Create H5P content folder.
+        $filename = 'fake.png';
+
+        $file = $tempfolder . '/' . $filename;
+        touch($file);
+
+        $this->assertFileExists($file);
+        // Save file in a temporary location.
+        $tempfile = editor_framework::saveFileTemporarily($file);
+        $this->assertIsObject($tempfile);
+        $this->assertFileExists($file);
+        $this->assertFileExists($tempfile->dir . '/' . $tempfile->fileName);
+    }
+
+    /**
+     * Test that the method getLibraries get the specified libraries or all the content types (runnable = 1).
+     */
+    public function test_getLibraries() {
+        $this->resetAfterTest(true);
+
+        $generator = \testing_util::get_data_generator();
+        $h5p_generator = $generator->get_plugin_generator('core_h5p');
+
+        // Generate some h5p related data.
+        $data = $h5p_generator->generate_h5p_data();
+
+        $expectedlibraries = [];
+        foreach ($data as $key => $value) {
+            if (isset($value->data)) {
+                $value->data->name = $value->data->machinename;
+                $value->data->majorVersion = $value->data->majorversion;
+                $value->data->minorVersion = $value->data->minorversion;
+                $expectedlibraries[$value->data->title] = $value->data;
+            }
+        }
+        ksort($expectedlibraries);
+
+        // Get all libraries.
+        $libraries = $this->editor_framework->getLibraries();
+        foreach ($libraries as $library) {
+            $actuallibraries[] = $library->title;
+        }
+        sort($actuallibraries);
+
+        $this->assertEquals(array_keys($expectedlibraries), $actuallibraries);
+
+        // Get a subset of libraries.
+        $librariessubset = array_slice($expectedlibraries, 0, 4);
+
+        $actuallibraries = [];
+        $libraries = $this->editor_framework->getLibraries($librariessubset);
+        foreach ($libraries as $library) {
+            $actuallibraries[] = $library->title;
+        }
+
+        $this->assertEquals(array_keys($librariessubset), $actuallibraries);
+    }
+
+    /**
+     * Test that the method removeTemporarilySavedFiles deletes files in a temporary location.
+     */
+    public function test_removeTemporarilySavedFiles() {
+        $this->resetAfterTest(true);
+
+        // Create temp folder.
+        $tempfolder = make_request_directory(false);
+
+        // Create H5P folder.
+        $h5pfolder = $tempfolder . '/folder';
+        if (!check_dir_exists($h5pfolder, true)) {
+            throw new moodle_exception('error_creating_temp_dir', 'error', $h5pfolder);
+        }
+
+        // Create several subfolders and files inside folder.
+        $filesexpected = array();
+        $numfolders = random_int(2,5);
+        for ($numfolder = 1; $numfolder < $numfolders; $numfolder++) {
+            $foldername = '/folder' . $numfolder;
+            $newfolder = $h5pfolder . $foldername;
+            if (!check_dir_exists($newfolder, true, true)) {
+                throw new moodle_exception('error_creating_temp_dir', 'error', $newfolder);
+            }
+            $numfiles = random_int(2, 5);
+            for ($numfile = 1; $numfile < $numfiles; $numfile++) {
+                $filename = '/file' . $numfile . '.ext';
+                touch($newfolder . $filename);
+                $filesexpected[] =  $foldername . $filename;
+            }
+        }
+
+        $this->assertDirectoryExists($h5pfolder);
+
+        $this->editor_framework::removeTemporarilySavedFiles($h5pfolder);
+
+        $this->assertDirectoryNotExists($h5pfolder);
+    }
+}
